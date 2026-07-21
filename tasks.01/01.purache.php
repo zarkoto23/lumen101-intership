71ab8196ac5b4c28c1da7bb007953b4f54349a99
Svetozar Stoev <zarkoto23@gmail.com>
2026-07-21 11:45:22 +0300

    task01

---
+++ tasks.01/01.purache.php
@@ -0,0 +1,27 @@
+<?php
+
+$productName = readline("Име на продукт: ");
+$priceForOne = readline("Цена за брой: ");
+$quantity = readline("Количество: ");
+$deliveryPrice = 5.99;
+
+$productsPrice = $priceForOne * $quantity;
+
+
+if ($productsPrice > 150) {
+    $deliveryPrice = 0;
+}
+
+if ($productsPrice > 200) {
+    $productsPrice *= 0.9;
+}
+
+echo "---------------------------------\n";
+echo "Продукт: $productName\n";
+echo "Единична цена: " . number_format($priceForOne, 2) . "\n";
+echo "Количество: $quantity\n";
+echo "Стойност на продуктите: " . number_format($productsPrice, 2) . "\n";
+echo "Доставка: " . number_format($deliveryPrice, 2) . "\n";
+echo "---------------------------------\n";
+echo "Крайна цена: " . number_format($productsPrice + $deliveryPrice, 2) . "\n";
+echo "---------------------------------\n";
