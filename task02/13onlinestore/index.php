1f973eb3e7077f13fc11d3506ec8945ffc5ae6cf
Svetozar Stoev <zarkoto23@gmail.com>
2026-07-22 14:54:45 +0300

    day2 taks13-online store

---
+++ task02/13onlinestore/index.php
@@ -0,0 +1,21 @@
+<?php
+include 'includes/header.php';
+include 'includes/navigation.php';
+?>
+
+<main>
+<nav class="main-nav">
+    <div class="logo">
+        <a href="index.php">Онлайн Магазин</a>
+    </div>
+    <ul class="nav-links">
+        <li><a href="index.php">Начало</a></li>
+        <li><a href="products.php">Продукти</a></li>
+        <li><a href="contact.php">Контакти</a></li>
+    </ul>
+</nav>
+
+   
+</main>
+
+<?php include 'includes/footer.php'; ?>
